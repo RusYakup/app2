57655f747034ecef5137b2248cec72524a39a12d
RusYakup <89799360+RusYakup@users.noreply.github.com>
2021-10-21 02:06:21 +0300

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,7 +1,21 @@
-from app import hello_world
+from app import app
+import unittest
 
-def test_answer():
-    """test hello_world func"""
-    right_answer = "Hello, world 2!!!"
-    assert right_answer == hello_world()
- 
+
+class FlaskAppTest(unittest.TestCase):
+    def setUp(self):
+        """Set up test application client"""
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        """Assert that user successfully lands on homepage"""
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
