e3912aec0d71afae1633eab97b034dab5ffc6952
RusYakup <89799360+RusYakup@users.noreply.github.com>
2021-10-21 00:37:51 +0300

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -2,6 +2,6 @@ from app import hello_world
 
 def test_answer():
     """test hello_world func"""
-    right_answer = "Hello, world !!!"
+    right_answer = "Hello, world 2!!!"
     assert right_answer == hello_world()
  
